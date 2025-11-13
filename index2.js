9e4a7b5bcb54d57863a271a7ae5b27f957626e91
etikala_archana <archanaetikala51@gmail.com>
2025-11-13 23:17:56 +0530

    Add a login file

---
+++ index2.js
@@ -0,0 +1,13 @@
+
+function signup(userName) {
+    // Predefined list of users
+    let users = ["john", "alex", "mike", "sara"];
+
+    if (users.includes(userName)) {
+        return "User Already Registered, Please Login";
+    } else {
+        users.push(userName);
+        return "Signup Successful, Please Login";
+    }
+}
+
